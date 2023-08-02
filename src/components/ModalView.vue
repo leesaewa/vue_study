1a6d175916acdd1f54e71af2e11b07f92211b687
leesaewa <saewa0424@gmail.com>
2023-08-02 21:27:07 +0900

    modal commit

---
+++ src/components/ModalView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    class="modal-overlay"
+    @click="$emit('close')"
+    style="
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #000000da;
+    "
+  >
+    <div
+      class="modal-inner"
+      style="
+        text-align: center;
+        background-color: white;
+        height: 500px;
+        width: 500px;
+        margin-top: 10%;
+        padding: 60px 0;
+        border-radius: 20px;
+      "
+    >
+      <h2>modal title</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum natus
+        sed quas esse excepturi. Velit vero similique tempore dolor ducimus
+        tempora perferendis minus et neque dolorum molestiae officia labore ea
+        possimus veritatis inventore, repellat ullam odit obcaecati assumenda?
+        Nobis officia a voluptatum blanditiis quia veritatis temporibus
+        excepturi asperiores omnis ratione!
+      </p>
+      <button
+        @click="$emit('close')"
+        type="button"
+        style="
+          border: 1px solid skyblue;
+          width: 50px;
+          height: 50px;
+          background-color: cadetblue;
+          cursor: pointer;
+          font-size: 18px;
+          font-weight: bold;
+          border-radius: 50px;
+        "
+      >
+        X
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalView",
+};
+</script>
